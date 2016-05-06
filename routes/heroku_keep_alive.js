dd6e9496331cc1b90239cacefd15bfd53153ac36
Jon Lamb <jonnylamb@gmail.com>
2016-05-05 22:17:44 -0500

    fixed keep alive module

--- routes/heroku_keep_alive.js
+++ routes/heroku_keep_alive.js
@@ -2,23 +2,18 @@ var express = require('express');
 var http = require("http");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    //Heroku keep alive
-    var hour = new Date().getHours();
-    console.log("The hour is: " + hour);
+//Heroku keep alive
+var hour = new Date().getHours();
+console.log("The hour is: " + hour);
 
-    setInterval(function() {
-        if (hour <= 24) {
-            console.log("Beginning of Looping " + hour);
-            http.get("https://slackbot-2.herokuapp.com/");
-        }
-        var d = new Date();
-        hour = d.getHours();
-        console.log("End of Loop " + d + "-" + hour);
-    }, 600000); // every 5 minutes (3000000)
-    res.render('index', { title: 'Express' });
-  res.render('index', { title: 'Express' });
-});
+setInterval(function() {
+    if (hour <= 24) {
+        console.log("Beginning of Looping " + hour);
+        http.get("https://slackbot-2.herokuapp.com/");
+    }
+    var d = new Date();
+    hour = d.getHours();
+    console.log("End of Loop " + d + "-" + hour);
+}, 600000); // every 5 minutes (3000000)
 
 module.exports = router;
