69dfbf2a4482ee57529ac9a5e70b6f3c790e78c7
Jon Lamb <jonnylamb@gmail.com>
2016-05-29 17:29:35 -0500

    testing

--- routes/netsuite_email_capture.js
+++ routes/netsuite_email_capture.js
@@ -87,6 +87,7 @@ router.post('/', function (req, res, next) {
         console.log('Loop: ' + i);
         console.log('Text: ' + $(this).text());
         for (var k in message){
+            console.log(message[k].keyword);
             if ($(this).text() == message[k].keyword){
                 console.log('Found Keyword: ' + message[k].keyword);
                 number = i;
