22b5ef2c10dc3f3dbaecf22312a3a949ab000378
Jon Lamb <jonnylamb@gmail.com>
2016-06-25 01:04:50 -0500

    Testing netsuite request

--- routes/netsuite_listener.js
+++ routes/netsuite_listener.js
@@ -50,7 +50,7 @@ router.post('/', function (req, res, next) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log('Request: ' + req);
+    console.log('Request: ' + JSON.stringify(req));
     res.render('index', { title: 'Jon\'s Awesome Node App' });
 });
 
