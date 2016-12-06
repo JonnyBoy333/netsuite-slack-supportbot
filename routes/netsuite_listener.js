ea4ab261f7b077c1b5a93be850f78a1df433c1c1
Jon Lamb <jonnylamb@gmail.com>
2016-12-06 11:13:03 -0600

    changing bot name

--- routes/netsuite_listener.js
+++ routes/netsuite_listener.js
@@ -60,6 +60,7 @@ router.post('/', function (req, res, next) {
             console.log(userId);
             delete attachment.assigned;
             var slackAttachment = {
+                "username": "support",
                 "attachments": [attachment],
                 "channel": userId
             };
