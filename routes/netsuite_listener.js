b1c43b48d2018ee8e8a844dd1686530d97c343b6
jonlamb <jonnylamb@gmail.com>
2017-01-17 21:26:56 -0600

    Fixing asyncronous for loops

--- routes/netsuite_listener.js
+++ routes/netsuite_listener.js
@@ -46,34 +46,11 @@ function getUserId (name){
     })
 }
 
-// Promise.prototype.thenReturn = function(value) {
-//     return this.then(function() {
-//         return value;
-//     });
-// };
-//
-// function sendMessage(index) {
-//     return new Promise(function(resolve) {
-//         setTimeout(function() {
-//             console.log("Read file number " + (index +1));
-//             resolve();
-//         }, 500);
-//     });
-// }
-//
-// // The loop initialization
-// var len = slackMessages.length;
-// Promise.resolve(0).then(function loop(i) {
-//     // The loop check
-//     if (i < len) { // The post iteration increment
-//         return sendMessage(i).thenReturn(i + 1).then(loop);
-//     }
-// }).then(function() {
-//     console.log("All messages sent");
-// }).catch(function(e) {
-//     console.log("error", e);
-// });
-
+Promise.prototype.thenReturn = function(value) {
+    return this.then(function() {
+        return value;
+    });
+};
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
@@ -81,51 +58,114 @@ router.post('/', function (req, res, next) {
     console.log('body: ' + JSON.stringify(message));
     var slackMessages = message['slack_messages']
     //console.log('headers: ' + JSON.stringify(req.headers));
-    for (var i = 0; i < slackMessages.length; i++) {
-        var attachment = slackMessages[i];
-        if (i === 0) {
-            var dirtyMessage = attachment.fields[2].value;
-            if (dirtyMessage) {
-                var cleanMessage = sanitizeHtml(dirtyMessage, {
-                    allowedTags: [],
-                    allowedAttributes: []
-                });
-                //console.log('Clean message: ' + cleanMessage);
-                var trimmedMessage = cleanMessage.trim();
-                var removeBlanks = /[\r\n]{2,}/g;
-                var noBlankLinesMessage = trimmedMessage.replace(removeBlanks, '\r\n');
-                console.log('No Blanks: ' + noBlankLinesMessage);
-                attachment.fields[2].value = noBlankLinesMessage;
-            }
-        }
+    // for (var i = 0; i < slackMessages.length; i++) {
+    //     var attachment = slackMessages[i];
+    //     if (i === 0) {
+    //         var dirtyMessage = attachment.fields[2].value;
+    //         if (dirtyMessage) {
+    //             var cleanMessage = sanitizeHtml(dirtyMessage, {
+    //                 allowedTags: [],
+    //                 allowedAttributes: []
+    //             });
+    //             //console.log('Clean message: ' + cleanMessage);
+    //             var trimmedMessage = cleanMessage.trim();
+    //             var removeBlanks = /[\r\n]{2,}/g;
+    //             var noBlankLinesMessage = trimmedMessage.replace(removeBlanks, '\r\n');
+    //             console.log('No Blanks: ' + noBlankLinesMessage);
+    //             attachment.fields[2].value = noBlankLinesMessage;
+    //         }
+    //     }
+    //
+    //     if (message.type === 'casereply'){
+    //         getUserId(message.assigned)
+    //         .then(function(userId) {
+    //             console.log('User ID: ', userId);
+    //             var slackAttachment = {
+    //                 "attachments": [attachment],
+    //                 "channel": userId
+    //             };
+    //             console.log('RTM Slack Attachment: ' + JSON.stringify(slackAttachment));
+    //             rtmBot.say(slackAttachment);
+    //         })
+    //     } else if (message.type === 'newcase') {
+    //         var attachmentMessage = {
+    //             channel: '#support_cases',
+    //             //channel: '#testing',
+    //             username: 'support',
+    //             icon_emoji: ':support:',
+    //             attachments: [attachment]
+    //         };
+    //         console.log('Webhooks Slack Attachment: ' + JSON.stringify(attachmentMessage));
+    //         webhooksBot.sendWebhook(attachmentMessage,function(err,res) {
+    //             if (err) {
+    //                 console.log(err)
+    //             }
+    //         });
+    //     }
+    // }
 
-        if (message.type === 'casereply'){
-            getUserId(message.assigned)
-            .then(function(userId) {
-                console.log('User ID: ', userId);
-                var slackAttachment = {
-                    "attachments": [attachment],
-                    "channel": userId
-                };
-                console.log('RTM Slack Attachment: ' + JSON.stringify(slackAttachment));
-                rtmBot.say(slackAttachment);
-            })
-        } else if (message.type === 'newcase') {
-            var attachmentMessage = {
-                channel: '#support_cases',
-                //channel: '#testing',
-                username: 'support',
-                icon_emoji: ':support:',
-                attachments: [attachment]
-            };
-            console.log('Webhooks Slack Attachment: ' + JSON.stringify(attachmentMessage));
-            webhooksBot.sendWebhook(attachmentMessage,function(err,res) {
-                if (err) {
-                    console.log(err)
+    function sendMessage(i) {
+        return new Promise(function(resolve) {
+            var attachment = slackMessages[i];
+            if (i === 0) {
+                var dirtyMessage = attachment.fields[2].value;
+                if (dirtyMessage) {
+                    var cleanMessage = sanitizeHtml(dirtyMessage, {
+                        allowedTags: [],
+                        allowedAttributes: []
+                    });
+                    //console.log('Clean message: ' + cleanMessage);
+                    var trimmedMessage = cleanMessage.trim();
+                    var removeBlanks = /[\r\n]{2,}/g;
+                    var noBlankLinesMessage = trimmedMessage.replace(removeBlanks, '\r\n');
+                    console.log('No Blanks: ' + noBlankLinesMessage);
+                    attachment.fields[2].value = noBlankLinesMessage;
                 }
-            });
-        }
+            }
+
+            if (message.type === 'casereply'){
+                getUserId(message.assigned)
+                    .then(function(userId) {
+                        console.log('User ID: ', userId);
+                        var slackAttachment = {
+                            "attachments": [attachment],
+                            "channel": userId
+                        };
+                        console.log('RTM Slack Attachment: ' + JSON.stringify(slackAttachment));
+                        rtmBot.say(slackAttachment);
+                        resolve();
+                    })
+            } else if (message.type === 'newcase') {
+                var attachmentMessage = {
+                    channel: '#support_cases',
+                    //channel: '#testing',
+                    username: 'support',
+                    icon_emoji: ':support:',
+                    attachments: [attachment]
+                };
+                console.log('Webhooks Slack Attachment: ' + JSON.stringify(attachmentMessage));
+                webhooksBot.sendWebhook(attachmentMessage,function(err,res) {
+                    if (err) {
+                        console.log(err)
+                    }
+                });
+                resolve();
+            }
+        });
     }
+
+    // The loop initialization
+    var len = slackMessages.length;
+    Promise.resolve(0).then(function loop(i) {
+        // The loop check
+        if (i < len) { // The post iteration increment
+            return sendMessage(i).thenReturn(i + 1).then(loop);
+        }
+    }).then(function() {
+        console.log("All messages sent");
+    }).catch(function(e) {
+        console.log("error", e);
+    });
     res.end("NetSuite Listener");
 });
 
