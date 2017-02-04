6f3f902d716e288ad391f4e6efa2943276dcef7f
jonlamb <jonnylamb@gmail.com>
2017-02-03 23:01:48 -0600

    changing heroku keep alive

--- routes/heroku_keep_alive.js
+++ routes/heroku_keep_alive.js
@@ -7,12 +7,12 @@ var hour = new Date().getHours();
 console.log("The hour is: " + hour);
 
 setInterval(function() {
-    if (hour <= 24 && hour >= 7) {
+    //if (hour <= 24 && hour >= 7) {
         http.get("http://slackbot-2.herokuapp.com/");
-    }
+    //}
     var d = new Date();
     hour = d.getHours();
     console.log("The time is: " + d + "-" + hour);
-}, 600000); // every 5 minutes (3000000)
+}, 30000); // every 5 minutes (3000000)
 
 module.exports = router;
