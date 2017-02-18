a6f14b3d001463d5b0b481bfaf31974563730f78
jonlamb <jonnylamb@gmail.com>
2017-02-18 09:29:06 -0600

    disabled time logging in keep alive on dev env

--- routes/heroku_keep_alive.js
+++ routes/heroku_keep_alive.js
@@ -9,12 +9,15 @@ console.log("The hour is: " + hour);
 setInterval(function() {
     if (process.env.NODE_ENV === 'Production') {
         https.get("https://netsuite-slack-supportbot.herokuapp.com/");
+        var d = new Date();
+        hour = d.getHours();
+        console.log("The time is: " + d + "-" + hour);
     } else {
-        //https.get("https://netsuite-slack-supportbot-dev.herokuapp.com/");
+        // https.get("https://netsuite-slack-supportbot-dev.herokuapp.com/");
+        // var d = new Date();
+        // hour = d.getHours();
+        // console.log("The time is: " + d + "-" + hour);
     }
-    var d = new Date();
-    hour = d.getHours();
-    console.log("The time is: " + d + "-" + hour);
 }, 30000); // every 5 minutes (3000000)
 
 module.exports = router;
