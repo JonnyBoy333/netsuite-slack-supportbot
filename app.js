92e82ce2a69472d7eba8ebc02a1f0eeed3b59406
jonlamb <jonnylamb@gmail.com>
2017-03-23 19:03:41 -0500

    fixed body parser issue

--- app.js
+++ app.js
@@ -67,7 +67,6 @@ var apiRouter = require('./api');
 
 var app = express();
 
-controller.createWebhookEndpoints(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -86,6 +85,9 @@ app.use('/api', apiRouter);
 app.use('/', index);
 app.use('/privacypolicy', privacypolicy);
 app.use('/contact', contact);
+
+//Create slackbot button endpoints server
+controller.createWebhookEndpoints(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
