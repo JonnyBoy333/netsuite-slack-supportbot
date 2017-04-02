3b21ff206def2b2393ec47624e7f5298af67bb39
jonlamb <jonnylamb@gmail.com>
2017-04-02 10:48:23 -0500

    Fixing message message break

--- routes/slacklistener.js
+++ routes/slacklistener.js
@@ -485,7 +485,7 @@ controller.hears([searchReg],['direct_message','direct_mention','mention'],funct
                                         var noBlankLinesMessage = trimmedMessage.replace(removeBlanks, '\r\n');
                                         console.log('i', i);
                                         console.log('No Blanks: ' + noBlankLinesMessage);
-                                        console.log('No Blanks and Intro Length', intro.length + 3 + noBlankLinesMessage.substr(0, 3990 - intro.length).length + 13);
+                                        console.log('No Blanks and Intro Length', intro.length + 3 + noBlankLinesMessage.substr(0, 3980 - intro.length).length + 13);
                                         if (noBlankLinesMessage.length + intro.length > 3980) {
                                             body[i].message = intro + '```' + noBlankLinesMessage.substr(0, 3980 - intro.length) + ' (more)...```';
                                         } else {
